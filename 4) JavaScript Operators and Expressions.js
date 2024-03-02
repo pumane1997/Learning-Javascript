b743dcd062bff096f07eab611dee5136e1e4e726
Pratik Mane <pumane1997@gmail.com>
2024-03-02 16:31:10 +0530

    JavaScript Operators and Expressions

---
+++ 4) JavaScript Operators and Expressions.js
@@ -0,0 +1,57 @@
+console.log("Arithmetic operators in JS")
+
+let a = 45
+let b = 4
+
+console.log("a + b =", a+b)
+console.log("a - b =", a-b)
+console.log("a / b =", a/b) // true division
+console.log("a * b =", a*b)
+
+console.log("a ** b =", a**b) // exponent
+console.log("a%b =", a%b) // remainder
+
+console.log("a++ =", a++)
+console.log(a)
+
+console.log("a-- =", a--)
+console.log(a)
+
+console.log("++a =", ++a)
+console.log(a)
+
+console.log("--a =", --a)
+console.log(a)
+
+
+
+console.log("Assignment operators in JS")
+
+let c = 5
+c += 2
+console.log(c) // same as c = c+2
+
+c -= 2
+console.log(c)
+
+c *= 2
+console.log(c)
+
+c /= 2
+console.log(c)
+
+c %= 2
+console.log(c)
+
+c **= 2
+console.log(c)
+
+
+
+console.log("Comparison operators in JS")
+
+console.log("Logical operators in JS")
+/* 
+This is a 
+multiline comment
+*/ 
