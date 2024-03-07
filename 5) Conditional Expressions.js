aba20935cd192d0e5e9a415967d6d3b65aac113b
Pratik <pum@adatis.uk.co>
2024-03-07 11:48:17 +0530

    Conditional Expressions

---
+++ 5) Conditional Expressions.js
@@ -0,0 +1,23 @@
+//node "5) Conditional Expressions.js"
+
+let a = "45"
+
+console.log(typeof a)
+
+a = Number.parseInt(a)
+
+console.log(typeof a)
+
+if (a<0){
+    console.log("This age is invalid")
+}
+else if(a<9){
+    console.log("You are a kid")
+}
+else if(a>9 && a<18){
+    console.log("Teen")
+}
+else{
+    console.log("You can drive")
+}
+
